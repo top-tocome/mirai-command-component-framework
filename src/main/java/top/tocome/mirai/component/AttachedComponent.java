3a6550c8ae8cc86e0ee8c56944c99fc410b5622b
remu <986563206@qq.com>
2021-08-03 09:19:45 +0800

    extract commandComponent

--- src/main/java/top/tocome/mirai/component/AttachedComponent.java
+++ src/main/java/top/tocome/mirai/component/AttachedComponent.java
@@ -2,79 +2,36 @@ package top.tocome.mirai.component;
 
 import net.mamoe.mirai.contact.Contact;
 import net.mamoe.mirai.event.Event;
-import top.tocome.mirai.control.CommandSet;
+import net.mamoe.mirai.event.events.MessageEvent;
 
 /**
  * 附加功能型组件
  */
-public abstract class AttachedComponent extends AbstractComponent {
+public abstract class AttachedComponent extends CommandComponent {
 
-    public AttachedComponent() {
-        commandInit();
-    }
+    protected MessageEvent event;
 
-    /**
-     * 组件的初始化
-     *
-     * @implSpec 指令集的初始化
-     */
-    protected abstract void commandInit();
-
-    /**
-     * 指令集
-     */
-    public CommandSet commandSet;
-
-    /**
-     * 指令消息
-     */
-    protected String commandMessage;
-
-    /**
-     * 调用该组件
-     *
-     * @param commandMessage null为普通功能,非空为指令
-     */
-    public boolean invoke(Event event, String commandMessage) {
-        this.commandMessage = commandMessage;
-        return super.invoke(event);
+    @Override
+    protected boolean setEventType(Event event) {
+        if (event instanceof MessageEvent) {
+            this.event = (MessageEvent) event;
+            return true;
+        }
+        return false;
     }
 
     @Override
-    public boolean invoke(Event event) {
-        return invoke(event, null);
+    protected boolean commandNext(String commandMessage) {
+        return false;
     }
 
     @Override
-    protected boolean enable() {
-        return commandMessage != null ? command() : common();
+    protected boolean disable() {
+        return false;
     }
 
-    /**
-     * 调用该组件的普通功能
-     */
-    protected abstract boolean common();
-
-    /**
-     * 调用该组件的指令集
-     */
-    protected boolean command() {
-        if (commandSet != null && commandSet.match(commandMessage)) return true;
-        return commandNext();
+    @Override
+    protected Contact getSubject() {
+        return event.getSubject();
     }
-
-    /**
-     * 调用下一个命令集
-     */
-    protected abstract boolean commandNext();
-
-    /**
-     * 获取消息来源
-     */
-    public abstract Contact getSubject();
-
-    /**
-     * 获取消息事件
-     */
-    protected abstract Event getEvent();
 }
---
+++ src/main/java/top/tocome/mirai/component/CommandComponent.java
@@ -0,0 +1,78 @@
+package top.tocome.mirai.component;
+
+import net.mamoe.mirai.contact.Contact;
+import net.mamoe.mirai.event.Event;
+import top.tocome.mirai.control.CommandSet;
+
+/**
+ * 包含消息指令的组件
+ */
+public abstract class CommandComponent extends AbstractComponent {
+
+    public CommandComponent() {
+        commandInit();
+    }
+
+    /**
+     * 该组件的指令集
+     */
+    protected CommandSet commandSet;
+
+    /**
+     * @implSpec 指令集的初始化
+     */
+    protected abstract void commandInit();
+
+
+    /**
+     * 指令消息内容
+     */
+    private String commandMessage;
+
+    /**
+     * 调用该组件
+     *
+     * @param event          mirai事件
+     * @param commandMessage null为普通调用,非空为指令匹配
+     * @return 调用结果
+     */
+    public boolean invoke(Event event, String commandMessage) {
+        this.commandMessage = commandMessage;
+        return super.invoke(event);
+    }
+
+    @Override
+    public boolean invoke(Event event) {
+        return invoke(event, null);
+    }
+
+    @Override
+    protected boolean enable() {
+        return commandMessage != null ? command() : common();
+    }
+
+    /**
+     * 该组件的普通功能
+     */
+    protected abstract boolean common();
+
+    /**
+     * 匹配该组件的指令集
+     */
+    private boolean command() {
+        if (commandSet != null && commandSet.match(commandMessage)) return true;
+        return commandNext(commandMessage);
+    }
+
+    /**
+     * 指令集匹配失败后调用
+     */
+    protected abstract boolean commandNext(String commandMessage);
+
+    /**
+     * 获取消息来源
+     *
+     * @apiNote 主要供指令action发送bot消息
+     */
+    protected abstract Contact getSubject();
+}
